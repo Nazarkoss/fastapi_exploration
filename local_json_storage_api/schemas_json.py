a0199d17e7a6c75da8f2b65ad217e96f9129fbcd
nazar <kostyuch.ua@gmail.com>
2022-11-29 22:18:37 +0000

    Debug Cercle Ci
    Debug test on CI
    
    Update schemas_json.py.py
    
    Debug test on CI
    
    Update schemas_json.py.py

--- local_json_storage_api/schemas_json.py
+++ local_json_storage_api/schemas_json.py
@@ -42,7 +42,10 @@ class Config:
 
 
 def load_db() -> list[CarOutput]:
-    with open("../cars.json") as f:
+    #import os
+    #raise ValueError(os.getcwd())
+    with open("cars.json") as f:
+#     with open("../cars.json") as f:
         return [CarOutput.parse_obj(obj) for obj in json.load(f)]
 
 
