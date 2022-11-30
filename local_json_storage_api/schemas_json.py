c278d42e4883881dcd580ae8e596338c48d7efa1
Nazar <117595266+Nazarkoss@users.noreply.github.com>
2022-11-30 12:03:24 +0000

    Update schemas_json.py

--- local_json_storage_api/schemas_json.py
+++ local_json_storage_api/schemas_json.py
@@ -42,9 +42,10 @@ class Config:
 
 
 def load_db() -> list[CarOutput]:
-    import os
-    raise ValueError(os.getcwd())
-    with open("../cars.json") as f:
+    #import os
+    #raise ValueError(os.getcwd())
+    with open("cars.json") as f:
+#     with open("../cars.json") as f:
         return [CarOutput.parse_obj(obj) for obj in json.load(f)]
 
 
